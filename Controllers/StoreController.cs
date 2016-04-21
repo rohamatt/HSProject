26c2e5252e799e1d4a2b913dac9de5f1b9c097f7
rohamatt <MatthewRo97@gmail.com>
2016-04-21 15:34:39 -0500

    new

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+
+        // GET: Store
+    public string Index()
+        {
+            return "Hello From Store.Index()";
+        }
+        //
+        //GET: /Store/Browse
+        public ActionResult Browse (string genre)
+        {
+            var genreModel = new Genre { name = genre };
+            return View(genreModel);
+        }
+        //
+        //GET:/Store/Details
+        public ActionResult Details( int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+        
+    }
+}
